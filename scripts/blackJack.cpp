ed3c2ef5da5efdf5108228c91719e1f7a6d693ba
Corey337337 <egorvasilevic95@gmail.com>
2026-07-03 15:20:27 +0300

    add class Hand

--- scripts/blackJack.cpp
+++ scripts/blackJack.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "time.h"
+
 using namespace std;
 
 class Card{
@@ -18,6 +21,50 @@ public:
         cout << suit << " " << rank << " - " << points << "points" << endl;
     }
 }
+
+class Hand{
+private:    
+    Vector<Card> my_hand;
+public:
+    Hand(vector<Card> m):my_hand{m}{}
+
+    void take_card(vector<Card>& all_cards){
+
+        int card = rand() % all_cards.size();
+        my_hand.push_back(all_cards[card]);
+
+        vector<Card>::iterator it = all_cards.begin();
+        advance(it, card);
+
+        all_cards.erase(it);
+    }
+
+    int AllPoints(){
+        
+        int p = 0;
+
+        for (size_t i = 0; i < my_hand.size(); i++)
+        {
+	        p += my_hand[i].getpoints();
+        }
+        cout << endl;
+
+        cout << "All points: " << p << endl;
+        return p;
+    }
+
+    void ShowHand(){
+        for (size_t i = 0; i < my_hand.size(); i++)
+        {
+            my_hand[i].print();
+        }
+    }
+
+    void clear_hand(){
+        my_hand.clear();
+    }
+}
+
 
 int main(){
 
